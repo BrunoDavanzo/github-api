350870b1691ae7fe2c304e8d4677161cedb64587
Bruno Davanzo <79986396+BrunoDavanzo@users.noreply.github.com>
2021-08-23 21:12:22 -0300

    Inserido os compenentes em tela

---
+++ App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Avatar } from './components/Avatar';
+import { DivFlex } from './components/DivFlex';
+import { Header } from './components/Header';
+import { Information } from './components/Information';
+import { Partition } from './components/Partition';
+import { Repo } from './components/Repo';
+
+function App() {
+  const [dados, setDados] = useState({})
+  const [repos, setRepos] = useState([])
+  const [userName, setUserName] = useState('BrunoDavanzo')
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userName}`)
+      .then(response => response.json())
+      .then(data => setDados(data))
+  }, [userName])
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userName}/repos`)
+      .then(response => response.json())
+      .then(data => setRepos(data))
+  }, [userName])
+
+  return (
+    <>
+    <Header name={dados.name} location={dados.location} />
+      <DivFlex>
+
+        <Information name='followers' num={dados.followers} />
+        <Avatar url={dados.avatar_url} />
+        <Information name='following' num={dados.following} />
+      </DivFlex>
+
+      <DivFlex>
+        {repos.map((repo, index, ) =>
+          <Repo 
+            key={index}
+            name={repo.name}
+            url={repo.html_url}
+          />
+        )}
+      </DivFlex>
+    </>
+  );
+}
+
+export default App;
